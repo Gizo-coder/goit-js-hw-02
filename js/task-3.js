a743f8955199c25aab36dddac7eed7f03e83f755
Gizem Demirci <gizdemirci91@gmail.com>
2025-06-01 17:39:22 +0300

    finished

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,23 @@
-function getElementWidth (content = "0px", padding = "0px", border = "0px" ) { 
+function checkForSpam (message) { 
 
-    const contentWidth = parseFloat(content);
+    const lowerCaseMessage = message.toLowerCase();
 
-    const paddingWidth = parseFloat(padding); 
-    
-    const borderWidth = parseFloat(border); 
+    if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+        return true;
+    }
 
-    let totalWidth = contentWidth + 2 * (paddingWidth + borderWidth);
+    else {
+        return false;
+    }
 
-    return totalWidth;
  
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
